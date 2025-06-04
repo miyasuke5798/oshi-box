147f294828563ed636e4a9e5d88395f2989230c7
tenra <tetenchi@yahoo.co.jp>
2025-06-04 14:43:52 +0900

    fixed menu to bottom

--- components/layout/share_menu.tsx
+++ components/layout/share_menu.tsx
@@ -28,29 +28,33 @@ export const ShareMenu: React.FC = () => {
   ];
 
   return (
-    <ul className="px-3 py-4 flex justify-between gap-3">
-      {menuItems.map((item) => {
-        const isActive = pathname === item.href;
-        return (
-          <li key={item.label} className="w-full max-w-[120px]">
-            <Link
-              href={item.href}
-              className="flex flex-col items-center w-full gap-1"
-            >
-              {item.icon}
-              <div
-                className={
-                  isActive
-                    ? "bg-[#3ebff5] text-white flex items-center justify-center text-[10px] h-[23px] w-full rounded-sm"
-                    : "text-[#3ebff5] flex items-center justify-center text-[10px] h-[23px] w-full rounded-sm"
-                }
-              >
-                {item.label}
-              </div>
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
+    <div className="fixed bottom-0 left-0 right-0 bg-white shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.03)]">
+      <div className="container pb-4">
+        <ul className="px-3 py-3 flex justify-between gap-3">
+          {menuItems.map((item) => {
+            const isActive = pathname === item.href;
+            return (
+              <li key={item.label} className="w-full max-w-[120px]">
+                <Link
+                  href={item.href}
+                  className="flex flex-col items-center w-full gap-1"
+                >
+                  {item.icon}
+                  <div
+                    className={
+                      isActive
+                        ? "bg-[#3ebff5] text-white flex items-center justify-center text-[10px] h-[23px] w-full rounded-sm"
+                        : "text-[#3ebff5] flex items-center justify-center text-[10px] h-[23px] w-full rounded-sm"
+                    }
+                  >
+                    {item.label}
+                  </div>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
   );
 };
