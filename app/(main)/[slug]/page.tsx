d925a51feace0ce52b4decc23341914fb64de481
tenra <tetenchi@yahoo.co.jp>
2025-06-09 13:32:16 +0900

    update profile image size

--- app/(main)/[slug]/page.tsx
+++ app/(main)/[slug]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import { ShareMenu } from "@/components/layout/share_menu";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { UserIcon } from "@/components/svg/UserIcon";
 import { CircleUser, UsersRound } from "lucide-react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
@@ -11,12 +11,9 @@ export default function SlugPage() {
     <div className="mt-3 mb-16">
       <ShareMenu />
       <Card className="w-full mb-4">
-        <CardHeader className="text-sm font-normal">
-          このエリアはxxxさんだけに見えています
-        </CardHeader>
         <Link
           href="/settings/profile"
-          className="bg-white hover:bg-gray-50 border-b"
+          className="bg-white hover:bg-gray-50 border-b rounded-t-[.5rem]"
         >
           <CardContent className="flex flex-row items-center gap-3 py-4 px-6">
             <CircleUser strokeWidth={1.5} className="w-6 h-6" />
@@ -25,7 +22,7 @@ export default function SlugPage() {
         </Link>
         <Link
           href="/settings/oshi"
-          className="bg-white hover:bg-gray-50 rounded-[.5rem]"
+          className="bg-white hover:bg-gray-50 rounded-b-[.5rem]"
         >
           <CardContent className="flex flex-row items-center gap-3 py-4 px-6">
             <UsersRound strokeWidth={1.5} className="w-6 h-6" />
@@ -35,7 +32,7 @@ export default function SlugPage() {
       </Card>
       <Card className="w-full mb-4">
         <CardContent className="flex flex-row justify-items-start items-center gap-4 py-5 px-6">
-          <UserIcon className="w-28 h-28 border border-gray-300 rounded-full" />
+          <UserIcon className="w-26 h-26 border border-gray-300 rounded-full" />
           <h1 className="text-xl text-[#181818] mt-4 mb-2">xxx</h1>
         </CardContent>
       </Card>
